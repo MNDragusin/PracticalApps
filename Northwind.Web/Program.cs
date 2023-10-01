da6766812167e56da21a86e6668213d5c75e6049
Mihai Dragusin <dragusinmihai08@gmail.com>
2023-10-01 22:52:49 +0200

    added a static page

--- Northwind.Web/Program.cs
+++ Northwind.Web/Program.cs
@@ -1,7 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}else{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/hello", () => "Hello World!");
 
 app.Run();
 Console.WriteLine("This executes afer the web server has stopped!");
