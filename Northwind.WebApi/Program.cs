3c290ae2e0e81112ebd33052cca41c47d2fdb45b
Mihai Dragusin <dragusinmihai08@gmail.com>
2024-02-08 21:45:57 +0100

    enabled http loggin

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Mdk.Shared;
 using Northwind.WebApi.Repositories;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,19 +30,33 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
+builder.Services.AddHttpLogging(option =>{
+    option.LoggingFields = HttpLoggingFields.All;
+    option.RequestBodyLogLimit = 4096;
+    option.ResponseBodyLogLimit = 4096;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>{
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Northwind Service API Version 1");
+        c.SupportedSubmitMethods(new[] {
+            SubmitMethod.Get,
+            SubmitMethod.Post,
+            SubmitMethod.Put,
+            SubmitMethod.Delete
+        });
+    });
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseHttpLogging();
 app.MapControllers();
 
 app.Run();
